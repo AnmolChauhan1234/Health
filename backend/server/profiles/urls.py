c6e743666383cd83ded9072b9c79cb5f4db6c0ec
AnmolChauhan <acanmolchauhan1234@gmail.com>
2025-02-08 18:52:04 +0530

    add view profile views and urls and also update hospital model

--- backend/server/profiles/urls.py
+++ backend/server/profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UpdateProfileView, ResetPasswordView
+from .views import UpdateProfileView, ResetPasswordView, ViewProfileView
 
 urlpatterns = [
     path('update/', UpdateProfileView.as_view(), name='update_profile'),
-    path('reset-password/', ResetPasswordView.as_view(), name='reset_password')
+    path('reset-password/', ResetPasswordView.as_view(), name='reset_password'),
+    path('view-profile/', ViewProfileView.as_view(), name='view_profile')
 ]
