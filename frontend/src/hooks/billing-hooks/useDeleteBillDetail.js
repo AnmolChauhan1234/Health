ca472adae0ffc714236426f7f20d94e27eb5dc76
Anmol Rai <purevoidexists@gmail.com>
2025-02-22 00:50:19 +0530

    Refactor useDeleteBillDetail hook to manage multiple deletions and improve API call structure

--- frontend/src/hooks/billing-hooks/useDeleteBillDetail.js
+++ frontend/src/hooks/billing-hooks/useDeleteBillDetail.js
@@ -4,16 +4,19 @@ import { useState } from "react";
 function useDeleteBillDetail() {
 
   //states for data.
-  const [isDeleting , setIsDeleting] = useState(false);
+  const [deletingIds, setDeletingIds] = useState([]);
 
   const deleteBillDetail = async (id) => {
+    // Add the id to the deletingIds array
+    setDeletingIds((prev) => [...prev, id]); 
 
-    setIsDeleting(true);
+    // console.log("api", id);
 
     try {
-      const response = await api.delete("/payments/delete-bill-details/" , {id});
+      const response = await api.delete("/payments/delete-bill-details/" , { data: { id } });
 
       if(response.status === 200){
+        // console.log(response.data.message)
         return {success: true , message: response.data.message};
       } else {
         return {success: false , message: "Bill Not found!"}
@@ -22,8 +25,15 @@ function useDeleteBillDetail() {
     } catch (error) {
       console.log("Error deleting Bill Detail" , error);
       return {success: false , message: "server did not respond."}
+    } finally{
+      // Remove the id from the deletingIds array
+      setDeletingIds((prev) => prev.filter((deletingId) => deletingId !== id)); 
     }
-  }
+  };
+
+  // Check if a specific id is being deleted
+  const isDeleting = (id) => deletingIds.includes(id);
+
   return {deleteBillDetail, isDeleting};
 
 }
