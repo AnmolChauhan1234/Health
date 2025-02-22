32ae060ee618b4ee436b10a536a0a6761488e7d9
AnmolChauhan <acanmolchauhan1234@gmail.com>
2025-02-22 12:43:05 +0530

    `Removed ml_api app and replaced with ml_app, updated search_api to use ml_app endpoint, and added ShowProfilePictureView in profiles app`

--- backend/server/ml_app/urls.py
+++ backend/server/ml_app/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from .views import predict
 
 urlpatterns = [
-    path('predict/', predict
-         , name='predict'),
-    
+    path('predict/', predict, name='predict'),
 ]
